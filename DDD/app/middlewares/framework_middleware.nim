7f7d66c4d49337b71c49df85c1d9fc0ada1f4a0e
itsumura-h <dumblepy@gmail.com>
2020-06-29 12:16:28 +0900

    update gitignore

--- DDD/app/middlewares/framework_middleware.nim
+++ DDD/app/middlewares/framework_middleware.nim
@@ -4,6 +4,6 @@ from custom_headers_middleware import corsHeader
 
 template framework*() =
   checkCsrfToken(request).catch()
-  checkAuthToken(request).catch()
+  checkAuthToken(request).catch(ErrorAuthRedirect, "/login")
   if request.reqMethod == HttpOptions:
     route(render(""), [corsHeader()])
