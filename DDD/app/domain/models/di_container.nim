10876b0a78e020c0af35a242737ba46d4f43bafb
itsumura-h <dumblepy@gmail.com>
2020-07-12 22:33:51 +0900

    update di container

--- DDD/app/domain/models/di_container.nim
+++ DDD/app/domain/models/di_container.nim
@@ -1,13 +1,2 @@
-#[
-Sample
-
-import user/repositories/user_rdb_repository
-import user/repositories/user_json_repository
-
-type DiContainer* = tuple
-  userRepository: UserRdbRepository
-  # userRepository: UserJsonRepository
-
-]#
 
 type DiContainer* = tuple
