9765122b87432c0ff307bdf46636a37dd0dc2cf7
itsumura-h <dumblepy@gmail.com>
2020-07-15 09:10:17 +0900

    change view header

--- DDD/resources/layouts/application.nim
+++ DDD/resources/layouts/application.nim
@@ -4,10 +4,7 @@ import head
 proc applicationView*(this:View, title:string, body:string):string = tmpli html"""
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  $(headView())
-  <title>$title</title>
-</head>
+  $(headView(title))
 <body>
   $body
 </body>
