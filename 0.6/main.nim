819d0a3ec6ca8397318cbc598e7b81fccf1c046f
itsumura-h <dumblepy@gmail.com>
2020-10-12 05:45:07 +0900

    add 0.6

--- 0.6/main.nim
+++ 0.6/main.nim
@@ -1,18 +1,10 @@
 # framework
-import basolato/routing
-# middleware
-import app/middlewares/custom_headers_middleware
-import app/middlewares/framework_middleware
+import basolato
 # controller
 import app/controllers/welcome_controller
 
-settings:
-  port = Port(5000)
 
-routes:
-  # Framework
-  error Http404: http404Route
-  error Exception: exceptionRoute
-  before: framework
+var routes = newRoutes()
+routes.get("/", welcome_controller.index)
 
-  get "/": route(newWelcomeController(request).index(),[corsHeader(), secureHeader()])
+serve(routes)
