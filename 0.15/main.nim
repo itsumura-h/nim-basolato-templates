e119f8366043dfc8db89db9d1bbf350a927ee855
itsumura-h <dumblepy@gmail.com>
2023-12-12 21:55:04 +0900

    add 0.15

---
+++ 0.15/main.nim
@@ -0,0 +1,27 @@
+# framework
+import basolato
+# middleware
+import ./app/http/middlewares/session_middleware
+import ./app/http/middlewares/auth_middleware
+import ./app/http/middlewares/set_headers_middleware
+# controller
+import ./app/http/controllers/welcome_controller
+
+
+let routes = @[
+  Route.group("", @[
+    Route.gorge("", @[
+      Route.get("/", welcome_controller.index),
+    ])
+    .middleware(session_middleware.sessionFromCookie)
+    .middleware(auth_middleware.checkCsrfToken),
+
+    Route.group("/api", @[
+      Route.get("/index", welcome_controller.indexApi),
+    ])
+    .middleware(set_headers_middleware.setSecureHeaders)
+  ])
+  .middleware(set_headers_middleware.setCorsHeaders)
+]
+
+serve(routes)
