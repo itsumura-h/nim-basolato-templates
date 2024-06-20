fcd7de9d4f36f6987d10c6d2d712a73d4f37f8cc
itsumura-h <dumblepy@gmail.com>
2024-06-21 07:25:25 +0900

    wip 0.16.0

---
+++ 0.16/app/http/middlewares/auth_middleware.nim
@@ -0,0 +1,16 @@
+import std/asyncdispatch
+import basolato/middleware
+
+
+proc checkCsrfToken*(c:Context, p:Params):Future[Response] {.async.} =
+  let res = await checkCsrfToken(c.request, p)
+  if res.hasError:
+    return render(Http403, res.message)
+  return next()
+
+
+proc checkSessionId*(c:Context, p:Params):Future[Response] {.async.} =
+  let res = await checkSessionId(c.request)
+  if res.hasError:
+    return errorRedirect("/signin")
+  return next()
---
+++ 0.16/main.nim
@@ -0,0 +1,29 @@
+# framework
+import basolato
+# middleware
+import ./app/http/middlewares/session_middleware
+import ./app/http/middlewares/auth_middleware
+import ./app/http/middlewares/set_headers_middleware
+# controller
+import ./app/http/controllers/welcome_controller
+
+
+let routes = @[
+  Route.group("", @[
+    Route.group("", @[
+      Route.get("/", welcome_controller.index),
+    ])
+    .middleware(session_middleware.sessionFromCookie)
+    .middleware(auth_middleware.checkCsrfToken),
+
+    Route.group("/api", @[
+      Route.get("/index", welcome_controller.indexApi),
+    ])
+    .middleware(set_headers_middleware.setSecureHeaders)
+  ])
+  .middleware(set_headers_middleware.setCorsHeaders)
+]
+
+let settings = Settings.new()
+
+serve(routes, settings)
